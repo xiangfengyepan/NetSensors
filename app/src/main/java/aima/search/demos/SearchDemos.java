dfe51ab782a3caf2b91ea7137cc945c645e23397
xiangfengyepan <xiangfengyepan@gmail.com>
2025-03-10 17:35:10 +0100

    class aima

---
+++ app/src/main/java/aima/search/demos/SearchDemos.java
@@ -0,0 +1,15 @@
+
+package aima.search.demos;
+
+/**
+ * @author Ravi Mohan
+ *
+ */
+public class SearchDemos {
+	public static void main(String[] args) {
+		NQueensDemo.main(null);
+		EightPuzzleDemo.main(null);
+		CSPDemo.main(null);
+	}
+
+}
