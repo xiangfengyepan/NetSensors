5fa33fffc0321a6d4b97c36446f78438c8d9fefd
xiangfengyepan <xiangfengyepan@gmail.com>
2025-03-30 14:55:23 +0200

    inical state 2

--- app/src/main/java/IA/InitialState.java
+++ app/src/main/java/IA/InitialState.java
@@ -11,7 +11,12 @@ public final class InitialState {
     private static final double SENSORS_DEGREE = 4.;
     private static final Random random = new Random(469874635498745L);
 
-    public static void inilializeConnections(State state) {
+    public static void inilializeConnections(State state)
+    {
+        inilializeConnections2(state);
+    }
+
+    private static void inilializeConnections1(State state) {
         // long seed = random.nextLong();
         // random.setSeed(seed);
         // System.out.println("Seed: " + seed);
@@ -38,6 +43,29 @@ public final class InitialState {
                     index = (int) Math.round(n * Math.pow(random.nextDouble(), SENSORS_DEGREE));
                 } while (state.connectToSensor(srcId,
                         src.nearestSensors()[index]) == ConnectionResult.UnableToConnect);
+            }
+        }
+    }
+
+    private static void inilializeConnections2(State state) {
+        for (int srcId = 0; srcId < state.sensorsCount(); ++srcId) {
+            Sensor src = state.problem.sensor(srcId);
+
+            int distCenter = src.sqDistanceTo(state.problem.center(src.nearestCenters()[0]));
+            int distSensor = src.sqDistanceTo(state.problem.sensor(src.nearestSensors()[0]));
+
+            boolean connectToCenter = distCenter <= distSensor || random.nextInt(100) >= PROB_CONNECT_CENTER;
+
+            if (connectToCenter) {
+                for (int dstId : src.nearestCenters()) {
+                    if (state.connectToCenter(srcId, dstId) != ConnectionResult.UnableToConnect)
+                        break;
+                }
+            } else {
+                for (int sensorDstId : src.nearestSensors()) {
+                    if (state.connectToSensor(srcId, sensorDstId) != ConnectionResult.UnableToConnect)
+                        break;
+                }
             }
         }
     }
